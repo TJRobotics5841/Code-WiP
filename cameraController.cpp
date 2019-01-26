64337249ee7200198275e918ec8aa2066431e34c
gaben5841 <46634297+gaben5841@users.noreply.github.com>
2019-01-26 16:03:46 -0500

    added camera movement functionality

--- cameraController.cpp
+++ cameraController.cpp
@@ -6,10 +6,11 @@
 /*----------------------------------------------------------------------------*/
 
 #include <cameraserver/CameraServer.h>
+#include <cscore_oo.h>
 #include <frc/TimedRobot.h>
 #include <wpi/raw_ostream.h>
-//this is for possibly customizing camera vision
-#include <cscore_c.h>
+#include <Joystick.h>
+#include <frc/Servo.h>
 
 /**
  * Uses the CameraServer class to automatically capture video from a USB webcam
@@ -20,13 +21,37 @@
 class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override {
+
 #if defined(__linux__)
-    frc::CameraServer::GetInstance()->StartAutomaticCapture();
+  cs::UsbCamera videoBoi = frc::CameraServer::GetInstance()->StartAutomaticCapture();
+  //videoBoi.SetResolution(1080, 1920);
+
 #else
     wpi::errs() << "Vision only available on Linux.\n";
     wpi::errs().flush();
 #endif
   }
+  void TeleopPeriodic() {
+
+    bool button5;
+    button5 = joy.GetRawButton(5);
+
+    bool button6;
+    button6 = joy.GetRawButton(6);
+
+    if (button6) {
+      s_servo1.SetAngle(0.0);
+    } else if (button5) {
+      s_servo1.SetAngle(90.0);
+    }
+    else{
+      s_servo1.SetAngle(15.0);
+    }
+  }
+  private:
+  frc::Joystick joy{0};
+  frc::Servo s_servo1{6};
+
 };
 
 #ifndef RUNNING_FRC_TESTS
