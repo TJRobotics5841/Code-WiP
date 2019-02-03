39e443af26a580adbe38290e016ee2448f6e9a70
gaben5841 <46634297+gaben5841@users.noreply.github.com>
2019-02-03 18:21:10 -0500

    Update ArmController.cpp

--- ArmController.cpp
+++ ArmController.cpp
@@ -9,8 +9,10 @@
 #include <frc/Spark.h>
 #include <frc/TimedRobot.h>
 #include <frc/XboxController.h>
+#include <frc/GenericHID.h>
 
 /**
+
  * This sample program shows how to control a motor using a joystick. In the
  * operator control part of the program, the joystick is read and the value is
  * written to the motor.
@@ -19,19 +21,18 @@
  * range from -1 to 1 making it easy to work together.
  */
 class Robot : public frc::TimedRobot {
- public:
-  void TeleopPeriodic() override { 
-    m_motor1.Set(m_control.GetY(kLeft)/4);
-    m_motor2.Set(m_control.GetY(kLeft)/4);
-    m_motor3.Set(m_control.GetY(kRight)/2);
   
-    }
+ public: void TeleopPeriodic() override { 
+
+    m_motor1.Set(m_control.GetY(frc::GenericHID::JoystickHand::kLeftHand)/4);
+    m_motor2.Set(m_control.GetY(frc::GenericHID::JoystickHand::kRightHand)/2);
+
+    };
 
  private:
   frc::XboxController m_control{0};
   frc::Spark m_motor1{0};
   frc::Spark m_motor2{1};
-  frc::Spark m_motor3{2};
 };
 
 #ifndef RUNNING_FRC_TESTS
